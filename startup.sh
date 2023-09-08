9d7b6f67979cee25ed1cc5519f036bae250b258f
Shashank-Sund <shanksund@gmail.com>
2023-09-08 15:16:14 -0400

    upload startup.sh file
    this file needs to be run before setting up repo

---
+++ startup.sh
@@ -0,0 +1,10 @@
+conda create -n myenv python=3.11.4 pandas numpy matplotlib matplotlib-inline plotly seaborn
+
+conda activate myenv
+
+pip install alpaca-py
+pip install json
+pip install datetime
+pip install sklearn
+pip install scikit-learn
+python -m pip install aws-cdk-lib
