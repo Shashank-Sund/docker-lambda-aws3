4c33bf7ed05ee4ccd88a663b3e05d9ca088133a0
shaicity <shanksund@gmail.com>
2023-09-20 13:35:27 -0400

    Updated to use Yahoo Finance for ticker data instead of Alpaca-py. Now able to pull market price data from any stock and can pull data from much further back.

--- image/src/main.py
+++ image/src/main.py
@@ -1,9 +1,4 @@
 import numpy as np
-from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import CryptoBarsRequest
-from alpaca.data.requests import StockBarsRequest
-from alpaca.data.timeframe import TimeFrame
 import datetime
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -15,6 +10,7 @@ import boto3
 import io
 import base64
 import re
+import yfinance as yahooFinance
 
 def handler(event, context):
 
@@ -22,57 +18,35 @@ def handler(event, context):
     start_date = event['start_date']
     end_date = event['end_date']
     symbol = event['symbol'].split(',')
-    
-    # converts date inputs to integers
+
+    # separates the year, month, and day for both the start and end dates
     pattern = r'[/-]'
     startyear,startmonth,startday = int(re.split(pattern, start_date)[0]), int(re.split(pattern, start_date)[1]), int(re.split(pattern, start_date)[2])
     endyear,endmonth,endday = int(re.split(pattern, end_date)[0]), int(re.split(pattern, end_date)[1]), int(re.split(pattern, end_date)[2])
 
-    # No keys required for crypto data, connect to Alpaca client
-    # client = StockHistoricalDataClient()
-    client = CryptoHistoricalDataClient()
-    
-    # Creating request object
-    # request_params = StockBarsRequest(
-    #                         symbol_or_symbols=symbol,
-    #                         timeframe=TimeFrame.Day,
-    #                         start=datetime.datetime(startyear, startmonth, startday),
-    #                         end=datetime.datetime(endyear, endmonth, endday)
-    #                         )
-    request_params = CryptoBarsRequest(
-                            symbol_or_symbols=symbol,
-                            timeframe=TimeFrame.Day,
-                            start=datetime.datetime(startyear, startmonth, startday),
-                            end=datetime.datetime(endyear, endmonth, endday)
-                            )
-    
-    # API request to get data
-    # bars = client.get_stock_bars(request_params, feed = 'us')
-    bars = client.get_crypto_bars(request_params, feed = 'us')
-    # Convert to dataframe
-    df = bars.df
-    
-    # gets all the timestamps, converts them to date types, puts them in a list
-    ts_list = []
-    for i in range(0,len(df)):
-        ts_list.append(df.index[i][1].date())
-    
-    # create a new column with the new date types
-    df = df.assign(date = ts_list)
-    df['date'] = pd.to_datetime(df['date'])
-    
-    # Gets all the multi-part index items
-    x = []
-    for i in range(0,len(df)):
-        x.append(df.index[i])
+    startDate = datetime.datetime(startyear, startmonth, startday) # startDate , as per our convenience we can modify
+    endDate = datetime.datetime(endyear, endmonth, endday) # endDate , as per our convenience we can modify
 
-    # Gets all the symbols from each index item
-    def get_symbol(symbol):
-        return symbol[0]
+    def get_data(symbols, start, end):
 
-    # creates new column for the symbol alone based on the method above
-    y = list(map(get_symbol,x))
-    df['symbol'] = y
+        df_list = []
+        for current_symbol in symbols:
+            stock_ticker = yahooFinance.Ticker(current_symbol) # get stock ticker
+            df = stock_ticker.history(start=start, end=end) # get data from yahoo
+
+            # gets all the timestamps, converts them to date types, puts them in a list
+            ts_list = []
+            for i in range(0,len(df)):
+                ts_list.append(df.index[i].date())
+                
+            df = df.assign(date = ts_list) # create a new column 'date' with the new date types
+            df['symbol'] = current_symbol # create new column 'symbol' containing the ticker symbol
+
+            df_list.append(df) # add dataframe to list
+
+        return df_list
+
+    df = pd.concat(get_data(symbol, startDate, endDate)) # concat all data together from method
     
     # Rename columns
     df.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume', 'trade_count': 'Trade_count', 'vwap': 'Vwap', 'date': 'Date', 'symbol': 'Symbol',}, inplace=True)
@@ -87,11 +61,6 @@ def handler(event, context):
     # # fits data using scaler
     scaler = StandardScaler()
     df[['Open', 'High', 'Low', 'Close', 'Volume']] = scaler.fit_transform(df[['Open', 'High', 'Low', 'Close', 'Volume']]) 
-    # print(df)
-
-    # Initialize s3 resource
-    # s3 = boto3.resource('s3')
-    # bucket = s3.Bucket("dockerlambdaoutputs3")
 
     if len(symbol) == 1: # USED FOR SINGLE-STOCK ANALYSIS
 
@@ -107,13 +76,8 @@ def handler(event, context):
         # encodes message
         encoded = base64.b64encode(img_data.getvalue()).decode('utf-8')
 
-        # Send data to s3 code
-        # img_data.seek(0)
-        # s3_object_key = "{}_to_{}/Closing_Stock_Price_Over_Time.png".format(start_date, end_date) 
-        # bucket.put_object(Body=img_data, ContentType='image/png', Key=s3_object_key)
-
         # Box plot of Closing Stock Prices by Year'
-        df['Year'] = df['Date'].dt.year
+        df['Year'] = [x.year for x in df['Date']]
         sns.boxplot(x='Year', y='Close', data=df)
         plt.title('Closing Stock Prices by Year')
         plt.xlabel('Year')
@@ -159,21 +123,6 @@ def handler(event, context):
         # encodes message
         encoded5 = base64.b64encode(img_data5.getvalue()).decode('utf-8')
 
-        # Line chart of closing stock price for multiple companies over time
-        companies = symbol
-        plt.figure(figsize=(10, 6))
-        for company in companies:
-            company_df = df[df['Symbol']==company]
-            sns.lineplot(x='Date', y='Close', data=company_df, label=company)
-        plt.title('Closing Stock Price of Multiple Companies Over Time')
-        plt.xlabel('Date')
-        plt.ylabel('Closing Stock Price')
-        # Save figure as png
-        img_data6 = io.BytesIO()
-        plt.savefig(img_data6, format='png')
-        # encodes message
-        encoded6 = base64.b64encode(img_data6.getvalue()).decode('utf-8')
-
         html = """ <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -199,12 +148,9 @@ def handler(event, context):
         <img src=\'data:image/png;base64,{}\'>
         <br>
         <br>
-        <img src=\'data:image/png;base64,{}\'>
-        <br>
-        <br>
     </body>
     </html>
-        """.format(encoded,encoded2,encoded3,encoded4,encoded5, encoded6)
+        """.format(encoded,encoded2,encoded3,encoded4,encoded5)
         
         return {
             'statusCode': 200,
